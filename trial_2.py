2fd39ce0cf71a1ff86144bb3155552146f41baca
anagha-khandelwal <52505621+anagha-khandelwal@users.noreply.github.com>
2019-08-29 15:02:49 -0400

    Update trial_2.py

--- trial_2.py
+++ trial_2.py
@@ -5,8 +5,6 @@ import datetime
 import os
 import re
 
-#keeps saying list index is out of range 
-#also what for loop should i add?
 filename = r"C:\Users\khand_ptsvdpp\Downloads\cmder (1)\2OAU.pdb"
 pdb_lines = open(filename).readlines()
 new_file = r"C:\Users\khand_ptsvdpp\Downloads\cmder (1)\trial.csv" 
@@ -14,9 +12,10 @@ with open(new_file,'w') as newcvsfile:
 	for line in pdb_lines:
 		fields = line.split()
 		if fields[0] == 'ATOM' and fields[2] == 'CA' :
-				fields2 = line.split()
-	print("this is field",fields[5], fields[6], fields[7])
-	print("this is field2", fields2[5],fields2[6], fields2[7]) 
+				for lines2 in pdb_lines:
+					fields2 = line.split()
+					print("this is field",fields[5], fields[6], fields[7])
+					print("this is field2", fields2[5],fields2[6], fields2[7]) 
 			
 	newcvsfile.close()
 
